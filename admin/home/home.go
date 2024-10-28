97c1cb3fdd564f9e2ea380edd1a9b909230efa13
Bl4ky113 <bl4ky@proton.me>
2024-10-27 20:44:50 -0500

    First attempt to use gin templates

--- admin/home/home.go
+++ admin/home/home.go
@@ -4,8 +4,17 @@ import (
     "net/http"
 
     "github.com/gin-gonic/gin"
+
+     shrd_templates "multi_cms/shared/cms_templates"
 )
 
 func ViewHome (ctx *gin.Context) {
-    ctx.HTML(http.StatusOK, "admin_home.tmpl", gin.H{})
+    viewCtx := make(gin.H)
+
+    err := shrd_templates.GenerateViewContext(ctx, &viewCtx)
+    if err != nil {
+        // TODO: ADD cms_logging error log
+    }
+
+    ctx.HTML(http.StatusOK, "admin_home.tmpl", viewCtx)
 }
--- shared/cms_templates/cms_templates.go
+++ shared/cms_templates/cms_templates.go
@@ -1,1 +1,43 @@
 package cms_templates
+
+import (
+    "strings"
+
+    "github.com/gin-gonic/gin"
+)
+
+const (
+    ADMIN_HOME_PATH = "admin"
+)
+
+func GenerateViewContext (ctx *gin.Context, viewCtx *gin.H) error {
+    
+    // Get View Data
+    (*viewCtx)["view"] = gin.H{
+        "pathArray": generatePathArray(ctx.FullPath()),
+        "usrName": "AdminUSR",
+        "usrRole": "Admin",
+        "usrIcon": "U",
+    }
+
+    return nil
+}
+
+func generatePathArray (pathRawString string) []string {
+    pathArray := make([]string, 0, 0)
+    pathRawArray := strings.Split(pathRawString, "/")
+
+    for _, str := range pathRawArray {
+        if strings.Compare(str, ADMIN_HOME_PATH) == 0 {
+            pathArray = append(pathArray, "Home")
+
+            continue
+        }
+
+        if len(str) <= 0 { continue }
+
+        pathArray = append(pathArray, str)
+    }
+
+    return pathArray
+}
